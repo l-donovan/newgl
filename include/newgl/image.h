b646a5e66b487db405f1eb5967e3208708717dee
Luke Donovan <ladbuilder1@gmail.com>
2022-02-21 20:27:35 -0800

    more image format flexibility

--- include/newgl/image.h
+++ include/newgl/image.h
@@ -7,5 +7,13 @@
 #include <SDL2/SDL.h>
 #include <SDL_image.h>
 
+struct format_t {
+    GLint internal_format;
+    GLint format;
+    GLint type;
+};
+
 SDL_Surface* load_texture(const char* filename);
 GLuint bind_texture(SDL_Surface *res_texture);
+void debug_print_format(SDL_PixelFormat *format);
+format_t gl_tex_params(SDL_PixelFormat *format);
--- src/image.cpp
+++ src/image.cpp
@@ -9,24 +9,27 @@
 
 using std::string;
 
-void print_format(SDL_PixelFormat *format) {
-    char fmt_str[format->BitsPerPixel + 1];
-    fmt_str[format->BitsPerPixel] = 0;
+void debug_print_format(SDL_PixelFormat *format) {
+    Uint8 bpp = format->BitsPerPixel;
+
+    char *fmt_str = (char*) malloc(sizeof(char) * (bpp + 1));
+    fmt_str[bpp] = 0;
+
     unsigned int mask;
 
-    for (unsigned int i = 0; i < format->BitsPerPixel; ++i) {
+    for (unsigned int i = 0; i < bpp; ++i) {
         mask = 1 << i;
 
         if ((format->Rmask & mask) == mask)
-            fmt_str[format->BitsPerPixel - i - 1] = 'R';
+            fmt_str[bpp - i - 1] = 'R';
         else if ((format->Gmask & mask) == mask)
-            fmt_str[format->BitsPerPixel - i - 1] = 'G';
+            fmt_str[bpp - i - 1] = 'G';
         else if ((format->Bmask & mask) == mask)
-            fmt_str[format->BitsPerPixel - i - 1] = 'B';
+            fmt_str[bpp - i - 1] = 'B';
         else if ((format->Amask & mask) == mask)
-            fmt_str[format->BitsPerPixel - i - 1] = 'A';
+            fmt_str[bpp - i - 1] = 'A';
         else
-            fmt_str[format->BitsPerPixel - i - 1] = '-';
+            fmt_str[bpp - i - 1] = '-';
     }
 
     PLOGD << "Format: (MSB) " << string(fmt_str) << " (LSB)";
@@ -44,8 +47,38 @@ SDL_Surface* load_texture(const char* filename) {
     return res_texture;
 }
 
+format_t gl_tex_params(SDL_PixelFormat *pixel_format) {
+    GLint internal_format;
+    GLint format;
+    GLint type;
+
+    switch (pixel_format->format) {
+    case SDL_PIXELFORMAT_RGB888:
+        internal_format = GL_RGBA8;
+        format = GL_BGRA;
+        type = GL_UNSIGNED_INT_8_8_8_8_REV;
+        break;
+    case SDL_PIXELFORMAT_RGB24:
+        internal_format = GL_RGB8;
+        format = GL_RGB;
+        type = GL_UNSIGNED_BYTE;
+    default:
+        internal_format = GL_RGB;
+        format = GL_RGB;
+        type = GL_UNSIGNED_BYTE;
+        break;
+    }
+
+    return {
+        internal_format,
+        format,
+        type
+    };
+}
+
 GLuint bind_texture(SDL_Surface *res_texture) {
-    print_format(res_texture->format);
+    debug_print_format(res_texture->format);
+    struct format_t params = gl_tex_params(res_texture->format);
 
     GLuint texture_id;
 
@@ -57,20 +90,15 @@ GLuint bind_texture(SDL_Surface *res_texture) {
     // I have no idea why pixel data comes in as BGR(A) instead of
     // as RGB(A) on macOS, but it does
     glTexImage2D(
-        GL_TEXTURE_2D,               // Target
-        0,                           // Level
-        GL_RGBA8,                    // Internal format
-        res_texture->w,              // Width
-        res_texture->h,              // Height
-        0,                           // Border
-#if __APPLE__
-        GL_BGRA,                     // Format
-        GL_UNSIGNED_INT_8_8_8_8_REV, // Type
-#else
-        GL_RGBA,                     // Format
-        GL_UNSIGNED_BYTE,            // Type
-#endif
-        res_texture->pixels);        // Data
+        GL_TEXTURE_2D,          // Target
+        0,                      // Level
+        params.internal_format, // Internal format
+        res_texture->w,         // Width
+        res_texture->h,         // Height
+        0,                      // Border
+        params.format,          // Format
+        params.type,            // Type
+        res_texture->pixels);   // Data
 
     SDL_FreeSurface(res_texture);
 
